99ba53a3f4852b6186997c9aa8fcec5172ccdfd5
Christiaan Kras <c.kras@heerenveen.nl>
2019-12-04 16:03:00 +0100

    Cleanup

--- src/admin-layers.js
+++ src/admin-layers.js
@@ -34,7 +34,6 @@ $(document).ready(function() {
         };
         return () => {
             const fields = visibleFields[type];
-            // console.log(type, fields, layerType.val());
             _showFields(visibleFields[type], () => {
                 return layerType.val() == type;
             });
