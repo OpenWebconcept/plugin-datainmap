278a89d10693647811154c33098ddc9fdfada03a
Christiaan Kras <c.kras@heerenveen.nl>
2020-01-16 13:20:20 +0100

    Meta keys bij custom post type (layers) omgezet naar private

--- plugins/gh-datainmap/includes/metaboxes.php
+++ plugins/gh-datainmap/includes/metaboxes.php
@@ -46,30 +46,30 @@ function gh_dim_add_metabox_layer() {
 }
 
 function gh_dim_metabox_layer($post) {
-    $gh_dim_layer_type = get_post_meta( $post->ID, 'gh_dim_layer_type', true );
-    $gh_dim_layer_url = get_post_meta( $post->ID, 'gh_dim_layer_url', true );
-    $gh_dim_layer_name = get_post_meta( $post->ID, 'gh_dim_layer_name', true );
-    $gh_dim_layer_opacity = get_post_meta( $post->ID, 'gh_dim_layer_opacity', true );
-    $gh_dim_layer_maxtrixset = get_post_meta( $post->ID, 'gh_dim_layer_maxtrixset', true );
-    $gh_dim_kml_ignore_style = get_post_meta( $post->ID, 'gh_dim_kml_ignore_style', true );
+    $gh_dim_layer_type = get_post_meta( $post->ID, '_gh_dim_layer_type', true );
+    $gh_dim_layer_url = get_post_meta( $post->ID, '_gh_dim_layer_url', true );
+    $gh_dim_layer_name = get_post_meta( $post->ID, '_gh_dim_layer_name', true );
+    $gh_dim_layer_opacity = get_post_meta( $post->ID, '_gh_dim_layer_opacity', true );
+    $gh_dim_layer_maxtrixset = get_post_meta( $post->ID, '_gh_dim_layer_maxtrixset', true );
+    $gh_dim_kml_ignore_style = get_post_meta( $post->ID, '_gh_dim_kml_ignore_style', true );
     include GH_DIM_DIR . '/views/metabox-layer.php';
 }
 
 add_action( 'save_post', 'gh_dim_metabox_layer_save_postdata' );
 function gh_dim_metabox_layer_save_postdata($post_id) {
     $keys = array(
-        'gh_dim_layer_type',
-        'gh_dim_layer_url',
-        'gh_dim_layer_name',
-        'gh_dim_layer_maxtrixset',
-        'gh_dim_kml_ignore_style',
+        '_gh_dim_layer_type',
+        '_gh_dim_layer_url',
+        '_gh_dim_layer_name',
+        '_gh_dim_layer_maxtrixset',
+        '_gh_dim_kml_ignore_style',
     );
     foreach($keys as $key) {
         if(array_key_exists($key, $_POST)) {
             update_post_meta($post_id, $key, $_POST[$key]);
         }
     }
-    if(array_key_exists('gh_dim_layer_opacity', $_POST)) {
-        update_post_meta($post_id, 'gh_dim_layer_opacity', sprintf('%.2f', abs($_POST['gh_dim_layer_opacity'])));
+    if(array_key_exists('_gh_dim_layer_opacity', $_POST)) {
+        update_post_meta($post_id, '_gh_dim_layer_opacity', sprintf('%.2f', abs($_POST['_gh_dim_layer_opacity'])));
     }
 }
--- plugins/gh-datainmap/views/metabox-layer.php
+++ plugins/gh-datainmap/views/metabox-layer.php
@@ -5,7 +5,7 @@
                 <label for="gh_dim_layer_type">Type</label>
             </th>
             <td>
-                <select id="gh_dim_layer_type" name="gh_dim_layer_type">
+                <select id="gh_dim_layer_type" name="_gh_dim_layer_type">
                     <option value="KML" <?php selected('KML', $gh_dim_layer_type) ?>><?php _e( 'KML', 'gh-datainmap' ); ?></option>
                     <option value="GeoJSON" <?php selected('GeoJSON', $gh_dim_layer_type) ?>><?php _e( 'GeoJSON', 'gh-datainmap' ); ?></option>
                     <option value="OSM" <?php selected('OSM', $gh_dim_layer_type) ?>><?php _e( 'OpenStreetMap', 'gh-datainmap' ); ?></option>
@@ -18,7 +18,7 @@
                 <label for="gh_dim_layer_url"><?php _e( 'URL', 'gh-datainmap' ) ?></label>
             </th>
             <td>
-                <input type="url" name="gh_dim_layer_url" id="gh_dim_layer_url" value="<?php echo esc_attr( $gh_dim_layer_url ) ?>"  class="regular-text" />
+                <input type="url" name="_gh_dim_layer_url" id="gh_dim_layer_url" value="<?php echo esc_attr( $gh_dim_layer_url ) ?>"  class="regular-text" />
                 <p class="description"><?php _e( 'URL to the dataset.', 'gh-datainmap' ) ?></p>
             </td>
         </tr>
@@ -27,7 +27,7 @@
                 <label for="gh_dim_layer_name"><?php _e('Layer name', 'gh-datainmap') ?></label>
             </th>
             <td>
-                <input type="text" name="gh_dim_layer_name" id="gh_dim_layer_name" value="<?php echo esc_attr( $gh_dim_layer_name ) ?>" />
+                <input type="text" name="_gh_dim_layer_name" id="gh_dim_layer_name" value="<?php echo esc_attr( $gh_dim_layer_name ) ?>" />
                 <p class="description"><?php _e( 'The name of the layer to display.', 'gh-datainmap' ) ?></p>
             </td>
         </tr>
@@ -36,7 +36,7 @@
                 <label for="gh_dim_layer_opacity"><?php _e('Layer opacity', 'gh-datainmap') ?></label>
             </th>
             <td>
-                <input type="text" name="gh_dim_layer_opacity" id="gh_dim_layer_opacity" value="<?php echo esc_attr( $gh_dim_layer_opacity ) ?>" />
+                <input type="text" name="_gh_dim_layer_opacity" id="gh_dim_layer_opacity" value="<?php echo esc_attr( $gh_dim_layer_opacity ) ?>" />
                 <p class="description"><?php _e( 'Layer opacity, between 0 and 1.', 'gh-datainmap' ) ?></p>
             </td>
         </tr>
@@ -45,7 +45,7 @@
                 <label for="gh_dim_layer_maxtrixset"><?php _e('Matrixset', 'gh-datainmap') ?></label>
             </th>
             <td>
-                <input type="text" name="gh_dim_layer_maxtrixset" id="gh_dim_layer_maxtrixset" value="<?php echo esc_attr( $gh_dim_layer_maxtrixset ) ?>" class="regular-text" />
+                <input type="text" name="_gh_dim_layer_maxtrixset" id="gh_dim_layer_maxtrixset" value="<?php echo esc_attr( $gh_dim_layer_maxtrixset ) ?>" class="regular-text" />
                 <p class="description"><?php _e('E.g. EPSG:3857', 'gh-datainmap' ) ?></p>
             </td>
         </tr>
@@ -54,8 +54,8 @@
                 <label for="gh_dim_kml_ignore_style"><?php _e('KML Style', 'gh-datainmap') ?></label>
             </th>
             <td>
-                <input type="hidden" name="gh_dim_kml_ignore_style" value="0" />
-                <input type="checkbox" name="gh_dim_kml_ignore_style" id="gh_dim_kml_ignore_style" value="1" <?php checked(1, $gh_dim_kml_ignore_style, true) ?> />
+                <input type="hidden" name="_gh_dim_kml_ignore_style" value="0" />
+                <input type="checkbox" name="_gh_dim_kml_ignore_style" id="gh_dim_kml_ignore_style" value="1" <?php checked(1, $gh_dim_kml_ignore_style, true) ?> />
                 <?php _e('Yes, ignore provided KML styles', 'gh-datainmap') ?>
                 <p class="description"><?php _e( 'This disables KML styling.', 'gh-datainmap' ) ?></p>
             </td>
