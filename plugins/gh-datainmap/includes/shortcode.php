2c5b297bf378963ae5d647b086a7b1af45f80d9d
Christiaan Kras <c.kras@heerenveen.nl>
2020-02-26 13:54:54 +0100

    EUPL licentie toegevoegd

--- plugins/gh-datainmap/includes/shortcode.php
+++ plugins/gh-datainmap/includes/shortcode.php
@@ -1,4 +1,17 @@
 <?php
+/*
+* Copyright 2020 Gemeente Heerenveen
+*
+* Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+* You may not use this work except in compliance with the Licence.
+* You may obtain a copy of the Licence at:
+*
+* https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the Licence for the specific language governing permissions and limitations under the Licence.
+*/
 
 add_shortcode( 'datainmap', 'gh_dim_shortcode' );
 function gh_dim_shortcode($atts, $content = null) {
