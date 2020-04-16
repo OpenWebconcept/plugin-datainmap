cf33feb9fa79e891398339c175646b6f5720ee09
Christiaan Kras <c.kras@heerenveen.nl>
2020-04-16 11:05:52 +0200

    Functie voor samenstellen location layer gh_dim_get_location_layer()

--- plugins/gh-datainmap/includes/shortcode.php
+++ plugins/gh-datainmap/includes/shortcode.php
@@ -76,73 +76,7 @@ function gh_dim_shortcode($atts, $content = null) {
     }
 
     // Compose location layers
-    $location_layers = array_map(function($term) {
-        $locations = get_posts([
-            'post_type' => 'gh-dim-locations',
-            'posts_per_page' => -1,
-            'tax_query' => [
-                [
-                    'taxonomy' => 'gh-dim-location-types',
-                    'field' => 'term_id',
-                    'terms' => $term->term_id,
-                ]
-            ]
-        ]);
-        $features = array_map(function($post) use($term) {
-            $location_properties = wp_get_post_terms( $post->ID, 'gh-dim-location-properties', [
-                'fields' => 'ids'
-            ] );
-            $location = get_post_meta( $post->ID, '_gh_dim_location', true);
-            $location_type = get_post_meta( $post->ID, '_gh_dim_location_type', true);
-            $content_type = get_post_meta( $post->ID, '_gh_dim_location_content_type', true);
-            $title = get_the_title( $post );
-            $useAlternativeTitle = get_post_meta( $post->ID, '_gh_dim_location_alternative_title', true ) == 1;
-            $alternativeTitle = get_post_meta( $post->ID, '_gh_dim_location_alternative_title_text', true);
-            if($useAlternativeTitle && strlen($alternativeTitle) > 0) {
-                $title = $alternativeTitle;
-            }
-            $content_type_enum = gh_dim_content_type_enum( $content_type );
-            $feature = [
-                'location_type' => $location_type,
-                'location' => json_decode($location),
-                'location_properties' => $location_properties,
-                'feature_id' => $post->ID,
-                'title' => $title,
-                'term' => $term->slug,
-                'content_type' => $content_type_enum,
-            ];
-            if($content_type_enum == GH_DIM_CONTENT_TYPE_REDIRECT) {
-                $redirect_url = get_post_meta( $post->ID, '_gh_dim_location_redirect_url', true);
-                $feature['redirect'] = $redirect_url;
-            }
-            $line_color = get_post_meta( $post->ID, '_gh_dim_location_style_line_color', true);
-            $line_width = get_post_meta( $post->ID, '_gh_dim_location_style_line_width', true);
-            $fill_color = get_post_meta( $post->ID, '_gh_dim_location_style_fill_color', true);
-            if($line_color || $line_width || $fill_color) {
-                $feature['style'] = [
-                    'line_color' => $line_color,
-                    'line_width' => $line_width,
-                    'fill_color' => $fill_color,
-                ];
-            }
-            return $feature;
-        }, $locations);
-        $icon = null;
-        $icon_media_id = get_term_meta( $term->term_id, 'category-image-id', true );
-        if($icon_media_id) {
-            $icon = wp_get_attachment_image_src($icon_media_id, 'full')[0];
-        }
-        return [
-            'name' => $term->name,
-            'term_id' => $term->term_id,
-            'slug' => $term->slug,
-            'description' => $term->description,
-            'icon' => $icon,
-            'features' => $features,
-            'cluster' => get_term_meta( $term->term_id, 'cluster', true ),
-            'cluster_distance' => get_term_meta( $term->term_id, 'cluster_distance', true ),
-        ];
-    }, $terms);
+    $location_layers = array_map('gh_dim_get_location_layer', $terms);
 
     // Fetch all used location-properties tags
     $location_property_ids = array_map(function($location_layer) {
