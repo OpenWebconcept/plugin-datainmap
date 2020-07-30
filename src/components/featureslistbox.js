c9f20b5516eb17d685503b27fa6204b35216b882
Christiaan Kras <c.kras@heerenveen.nl>
2020-07-30 14:42:33 +0200

    Opmaak features-listbox + toetsenbord navigatie

--- src/components/featureslistbox.js
+++ src/components/featureslistbox.js
@@ -12,25 +12,104 @@
 * See the Licence for the specific language governing permissions and limitations under the Licence.
 */
 import React, {Component} from 'react';
-import { flattenFeatures } from '../util/feature';
-import { FEATURE_TYPE_BUILTIN } from '../constants';
+import { FEATURE_TYPE_BUILTIN, CONTENT_TYPE_POST, CONTENT_TYPE_REDIRECT } from '../constants';
+import classNames from 'classnames';
 import _ from 'lodash';
 
 class FeaturesListboxComponent extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentResult: null
+        };
+        this.headerId = _.uniqueId('gh-dim-features-listbox-title-');
+    }
+
+    keyboardNavigation(e) {
+        if(e.key == 'ArrowDown') {
+            e.preventDefault();
+            this.navigateUpDown(true);
+        }
+        if(e.key == 'ArrowUp') {
+            e.preventDefault();
+            this.navigateUpDown(false);
+        }
+        if(e.key == 'Enter' && this.state.currentResult !== null) {
+            e.preventDefault();
+            const feature = this.props.visibleFeatures[ this.state.currentResult ];
+            this.props.onSelectFeature({
+                feature: feature.getProperties(),
+                type: FEATURE_TYPE_BUILTIN
+            })
+        }
+    }
+
+    navigateUpDown(incr) {
+        if(!this.props.visibleFeatures) {
+            return;
+        }
+        const totalResults = this.props.visibleFeatures.length;
+        let nextResult;
+        // Select first result
+        if((this.state.currentResult === null || this.state.currentResult == totalResults - 1) && incr) {
+            nextResult = 0;
+        }
+        // Select last result
+        else if((this.state.currentResult === null || this.state.currentResult == 0) && !incr) {
+            nextResult = totalResults - 1;
+        }
+        // Select previous/next result
+        else {
+            nextResult = this.state.currentResult + (incr ? 1 : -1);
+        }
+        this.setState({currentResult: nextResult});
+    }
     render() {
         const features = this.props.visibleFeatures;
         const totalResults = features.length;
+        if(totalResults == 0) {
+            return null;
+        }
+        let activeDescendant;
+        let i = 0;
+        const results = features.slice(0, this.props.maxResults).map((feature) => {
+            const selected = i++ === this.state.currentResult;
+            const classes = classNames({
+                focused: selected,
+                redirect: feature.get('content_type') == CONTENT_TYPE_REDIRECT,
+                post: feature.get('content_type') == CONTENT_TYPE_POST
+            });
+            const id = _.uniqueId('gh-dim-features-listbox-item-');
+            if(selected) {
+                activeDescendant = id;
+            }
+            return <li key={i}
+                id={id}
+                aria-selected={selected ? 'true' : 'false'}
+                onClick={(e) => this.props.onSelectFeature({
+                    feature: feature.getProperties(),
+                    type: FEATURE_TYPE_BUILTIN
+                })}
+                role="option"
+                className={classes}>{feature.get('title')}</li>
+        });
+        const expanded = totalResults > 0 ? true : false;
+        
         return (
-            <div className="gh-dim-features-listbox">
-                <ul>
-                    {features.slice(0, this.props.maxResults).map((feature, key) => {
-                        return <li key={key} onClick={(e) => this.props.onSelectFeature({
-                            feature: feature.getProperties(),
-                            type: FEATURE_TYPE_BUILTIN
-                        })}>{feature.get('title')}</li>
-                    })}
-                </ul>
+            <div className="gh-dim-features-listbox"  tabIndex="0" onKeyDown={(e) => this.keyboardNavigation(e)}>
+                <section aria-labelledby={this.headerId}>
+                    <header>
+                        <h1 id={this.headerId}>Gevonden locaties <span>({totalResults > 100 ? '100+' : totalResults})</span></h1>
+                    </header>
+                    <div className="gh-dim-features-listbox-content">
+                        <ul aria-live="polite" role="listbox" aria-label="Locaties zichtbaar op de kaart" aria-activedescendant={activeDescendant} aria-expanded={expanded}>
+                            {results.map((result) => { return result })}
+                        </ul>
+                        {totalResults > this.props.maxResults &&
+                            <p className="gh-dim-features-listbox-description">{this.props.maxResults} van de {totalResults} resultaten zichtbaar. Zoom in om de resultaten te verfijnen.</p>}
+                    </div>
+                </section>
             </div>
         )
     }
@@ -39,7 +118,7 @@ class FeaturesListboxComponent extends Component {
 FeaturesListboxComponent.defaultProps = {
     visibleFeatures: [],
     onSelectFeature: _.noop,
-    maxResults: 100
+    maxResults: 25
 };
 
 export default FeaturesListboxComponent;
