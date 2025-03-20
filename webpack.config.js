4b0f7b3bb2d560e7a46f48728db266dc077e2bc3
Christiaan Kras <c.kras@heerenveen.nl>
2025-03-20 11:23:17 +0100

    Updated copyright date

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = {
     devtool: 'cheap-source-map'
 };
 
-function licenseText() { return `Copyright 2020-2023 Gemeente Heerenveen
+function licenseText() { return `Copyright 2020-2025 Gemeente Heerenveen
 
 Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
 You may not use this work except in compliance with the Licence.
